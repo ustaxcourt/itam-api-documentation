7cbcf2869ed30c66009e977d83267f8be5c13d28
Sharlet Claros <sharlet.claros.ctr@ustaxcourt.gov>
2025-10-03 08:06:14 -0400

    new branch. updated terraform files. updated readme. removed some files no longer in use.

--- azure-functions/index.js
+++ azure-functions/index.js
@@ -4,8 +4,11 @@ import './helloworld.js'; // Import your function(s) here
 import './testAuth.js';
 import './queryAssets.js';
 
+<<<<<<< HEAD
 import './queryAsset.js';
 
+=======
+>>>>>>> e2c2335 (new branch. updated terraform files. updated readme. removed some files no longer in use.)
 app.setup({
   enableHttpStream: true
 });
---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+
+}
+
+# Reference existing resource group
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+# Reference existing storage account
+data "azurerm_storage_account" "storage" {
+  name                = var.storage_account_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# Create Service Plan for Linux and Node.js v22
+resource "azurerm_service_plan" "plan" {
+  name                = "${var.function_app_name}-plan"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+# Create Application Insights instance
+resource "azurerm_application_insights" "insights" {
+  name                = "${var.function_app_name}-ai"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+# Create Azure Function App
+resource "azurerm_linux_function_app" "function" {
+  name                       = var.function_app_name
+  location                   = data.azurerm_resource_group.rg.location
+  resource_group_name        = data.azurerm_resource_group.rg.name
+  service_plan_id            = azurerm_service_plan.plan.id
+  storage_account_name       = data.azurerm_storage_account.storage.name
+  storage_account_access_key = data.azurerm_storage_account.storage.primary_access_key
+
+  site_config {
+    # optional remove if causing issues
+    application_insights_key = azurerm_application_insights.insights.instrumentation_key
+  }
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME               = "node"
+    WEBSITE_RUN_FROM_PACKAGE              = "1"
+    AzureWebJobsStorage                   = data.azurerm_storage_account.storage.primary_connection_string
+    APPINSIGHTS_INSTRUMENTATIONKEY        = azurerm_application_insights.insights.instrumentation_key
+    APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.insights.connection_string
+  }
+}
