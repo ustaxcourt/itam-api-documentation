5bd2c56a614c17a6b98d18d9b0fefb992ace91d9
sharletclaros <sharlet.claros.ctr@ustaxcourt.gov>
2025-10-22 07:13:12 -0400

    updated table to existing

--- azure-functions/getCodeVerifier.js
+++ azure-functions/getCodeVerifier.js
@@ -1,6 +1,6 @@
 import { TableClient } from '@azure/data-tables';
 
-const tableClient = TableClient.fromConnectionString(process.env.AzureWebJobsStorage, 'PKCEState');
+const tableClient = TableClient.fromConnectionString(process.env.AzureWebJobsStorage, process.env.TABLE_NAME);
 
 export async function getCodeVerifier(state) {
   const entity = await tableClient.getEntity('PKCE', state);
