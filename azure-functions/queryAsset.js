ae714a2fae328765bdbd712114a12f8e964f55be
mh137946 <Matthew.Hung.ctr@ustaxcourt.gov>
2025-11-03 16:00:54 -0800

    minor fix

--- azure-functions/queryAsset.js
+++ azure-functions/queryAsset.js
@@ -25,7 +25,7 @@ app.http('queryAsset', {
         };
       }
 
-      url = `${DATAVERSE_URL}/api/data/v9.2/crf7f_ois_asset_rela_item_orgs?$filter=crf7f_ois_asset_rela_item_orgid eq '${id}'&$expand=crf7f_ois_asset_entra_dat_userCurrentOw($select=crf7f_email,crf7f_jobtitle,crf7f_name,crf7f_isactive,crf7f_iscontractor, crf7f_entra_object_id,crf7f_phone)`;
+      let url = `${DATAVERSE_URL}/api/data/v9.2/crf7f_ois_asset_rela_item_orgs?$filter=crf7f_ois_asset_rela_item_orgid eq '${id}'&$expand=crf7f_ois_asset_entra_dat_userCurrentOw($select=crf7f_email,crf7f_jobtitle,crf7f_name,crf7f_isactive,crf7f_iscontractor, crf7f_entra_object_id,crf7f_phone)`;
 
       const response = await axios.get(url, {
         headers: {
@@ -65,51 +65,3 @@ app.http('queryAsset', {
     }
   }
 });
-
-function filterDictionary(dict) {
-  const cleaned = {};
-  const guidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i;
-
-  const allowedKeys = [
-    "_crf7f_ois_asset_dat_itemlookup_value@OData.Community.Display.V1.FormattedValue",
-    "_crf7f_fac_asset_ref_locationlookup_value@OData.Community.Display.V1.FormattedValue",
-    "crf7f_asset_item_status@OData.Community.Display.V1.FormattedValue",
-    "crf7f_phone_numbers",
-    "crf7f_asset_item_condition@OData.Community.Display.V1.FormattedValue",
-    "crf7f_service_activation",
-    "_crf7f_ois_asset_entra_dat_usercurrentow_value@OData.Community.Display.V1.FormattedValue",
-    "crf7f_os_version"
-  ];
-
-  const keyMap = {
-
-    "_crf7f_ois_asset_dat_itemlookup_value@OData.Community.Display.V1.FormattedValue": "assetName",
-    "_crf7f_fac_asset_ref_locationlookup_value@OData.Community.Display.V1.FormattedValue": "location",
-    "crf7f_asset_item_status@OData.Community.Display.V1.FormattedValue": "itemStatus",
-    "crf7f_phone_numbers": "phone",
-    "crf7f_asset_item_condition@OData.Community.Display.V1.FormattedValue": "condition",
-    "crf7f_service_activation": "activation",
-    "_crf7f_ois_asset_entra_dat_usercurrentow_value@OData.Community.Display.V1.FormattedValue": "user",
-    "crf7f_os_version": "osVersion"
-  };
-
-  for (const key in dict) {
-    const value = dict[key];
-
-    // Skip if key is not allowed
-    if (!allowedKeys.includes(key)) {
-      continue;
-    }
-
-    // Skip if value contains a GUID
-    if (typeof value === "string" && guidRegex.test(value)) {
-      continue;
-    }
-
-    // Use pretty key name
-    const prettyKey = keyMap[key] || key;
-    cleaned[prettyKey] = value;
-  }
-
-  return cleaned;
-}
