369484a4ad9db6c53e88d6575103fd119c0e45ca
mh137946 <Matthew.Hung.ctr@ustaxcourt.gov>
2025-11-06 10:57:10 -0800

    to pull out function into helper

---
+++ azure-functions/helperFunctions/userHelpers.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { getToken } from '../oauth.js';
+
+export function giveMeRowId(userid) {
+  const token = getToken();
+  let url = `${DATAVERSE_URL}/api/data/v9.2/crf7f_ois_asset_entra_dat_users?$filter=crf7f_name eq '${userid}'`;
+  let response = axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      Prefer: 'odata.include-annotations="OData.Community.Display.V1.FormattedValue"'
+    }
+  });
+  rowId = response.data["value"][0]["crf7f_ois_asset_entra_dat_userid"];
+  return rowId
+}
