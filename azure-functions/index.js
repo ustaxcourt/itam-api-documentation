5e06bfcd4902ad58cbcc74704d58c3682e089043
mh137946 <Matthew.Hung.ctr@ustaxcourt.gov>
2025-11-06 10:43:49 -0800

    added endpoint to index

--- azure-functions/index.js
+++ azure-functions/index.js
@@ -3,6 +3,7 @@ import { app } from '@azure/functions';
 import './helloworld.js'; // Import your function(s) here
 import './testAuth.js';
 import './queryAssets.js';
+import './assetAssignments.js';
 
 import './queryAsset.js';
 import './authTest.js';
