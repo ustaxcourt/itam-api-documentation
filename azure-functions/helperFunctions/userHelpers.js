5748816ff5c72b2913a4762522b0b1a3f97f5dff
mh137946 <Matthew.Hung.ctr@ustaxcourt.gov>
2025-11-06 11:14:04 -0800

    pulled out finished

--- azure-functions/helperFunctions/userHelpers.js
+++ azure-functions/helperFunctions/userHelpers.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
 import { getToken } from '../oauth.js';
 
-export function giveMeRowId(userid) {
-  const token = getToken();
+const { DATAVERSE_URL } = process.env;
+
+export async function giveMeRowId(userid) {
+  const token = await getToken();
+
   let url = `${DATAVERSE_URL}/api/data/v9.2/crf7f_ois_asset_entra_dat_users?$filter=crf7f_name eq '${userid}'`;
-  let response = axios.get(url, {
+  let response = await axios.get(url, {
     headers: {
       Authorization: `Bearer ${token}`,
       Accept: 'application/json',
       Prefer: 'odata.include-annotations="OData.Community.Display.V1.FormattedValue"'
     }
   });
-  rowId = response.data["value"][0]["crf7f_ois_asset_entra_dat_userid"];
+  let rowId = response.data["value"][0]["crf7f_ois_asset_entra_dat_userid"];
   return rowId
 }
