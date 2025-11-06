4a605a5181f27032c3d2c061af80da45a9889521
mh137946 <Matthew.Hung.ctr@ustaxcourt.gov>
2025-11-06 11:15:15 -0800

    big boom bam done

--- azure-functions/assetAssignments.js
+++ azure-functions/assetAssignments.js
@@ -32,7 +32,7 @@ app.http('assignments', {
 
       if (request.method === 'POST') {
         let userId = request.params.userid;
-        rowId = giveMeRowId(userId)
+        rowId = await giveMeRowId(userId);
         var body = {
           "crf7f_ois_asset_entra_dat_userCurrentOw@odata.bind": `crf7f_ois_asset_entra_dat_users(${rowId})`,
           "crf7f_asset_item_status": 0
--- azure-functions/helperFunctions/userHelpers.js
+++ azure-functions/helperFunctions/userHelpers.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { getToken } from '../oauth.js';
 
-export function giveMeRowId(userid) {
+export async function giveMeRowId(userid) {
   const token = getToken();
   let url = `${DATAVERSE_URL}/api/data/v9.2/crf7f_ois_asset_entra_dat_users?$filter=crf7f_name eq '${userid}'`;
-  let response = axios.get(url, {
+  let response = await axios.get(url, {
     headers: {
       Authorization: `Bearer ${token}`,
       Accept: 'application/json',
