1c99cf6e46af2978648d865ff39cd6eba6ae6448
Mike Marcotte <mike.marcotte@gmail.com>
2025-11-10 11:33:22 -0500

    cleanup index; test pre-commit

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@
  * Initial iteration of index file. As we test - will be updated and formatted to generate ITAM table data
  */
 
-
-
 console.log('DATAVERSE_URL from .env:', process.env.DATAVERSE_URL);
 
-const { getToken } = require('./auth/oauth');
+import { getToken } from './auth/oauth';
 
 async function testOAuth() {
   try {
     const token = await getToken();
     console.log('OAuth token received:\n', token);
   } catch (error) {
-    console.error('Failed to get token:', error.response?.data || error.message);
+    console.error(
+      'Failed to get token:',
+      error.response?.data || error.message,
+    );
   }
 }
 
 testOAuth();
-
