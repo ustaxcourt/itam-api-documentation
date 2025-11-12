59e0bfcaace36c1d265839b7a6eb4979a7aed45c
sharletclaros <23251609+sharletclaros@users.noreply.github.com>
2025-11-12 12:08:40 -0500

    added config for coverage 25%

--- jest.config.js
+++ jest.config.js
@@ -4,16 +4,24 @@ export default {
   },
   testEnvironment: 'node',
 
-
   collectCoverage: true,
   collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/node_modules/**",
-    "!**/coverage/**",
-    "!**/*.test.{js,jsx,ts,tsx}",
-    "!**/*config.js"
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!**/*.test.{js,jsx,ts,tsx}',
+    '!**/*config.js',
   ],
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov", "html"]
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
 
+  // Enforcing minimum coverage of 25%
+  coverageThreshold: {
+    global: {
+      branches: 25,
+      functions: 25,
+      lines: 25,
+      statements: 25,
+    },
+  },
 };
