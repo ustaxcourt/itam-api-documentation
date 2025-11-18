9ca4ec3f096f80a73594a5ff80ce6afbb1c2b583
mh137946 <Matthew.Hung.ctr@ustaxcourt.gov>
2025-11-18 08:13:20 -0800

    error

--- azure-functions/apiController/getDataverseTokenHandler.js
+++ azure-functions/apiController/getDataverseTokenHandler.js
@@ -3,13 +3,7 @@ import { getToken } from '../oauth.js';
 export async function dataverseTokenHandler() {
   const token = await getToken();
   if (!token) {
-    return {
-      status: 403,
-      jsonBody: {
-        error: 'Unauthorized',
-        details: 'Dataverse internal token is missing or invalid.',
-      },
-    };
+    throw new Error('no token found');
   }
   return token;
 }
