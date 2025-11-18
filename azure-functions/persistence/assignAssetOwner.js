172643385c7b7c9a4768904d4d75b3b550ce8de1
sharletclaros <23251609+sharletclaros@users.noreply.github.com>
2025-11-18 12:13:11 -0500

    removed unecessary mod to bind script in assignAssetOwner body

--- azure-functions/persistence/assignAssetOwner.js
+++ azure-functions/persistence/assignAssetOwner.js
@@ -7,7 +7,7 @@ export async function assignAssetOwner(userId, assetId) {
   const rowId = await getUserById(userId);
 
   const body = {
-    'crf7f_ois_asset_entra_dat_userCurrentOw@odata.bind': `/crf7f_ois_asset_entra_dat_users(${rowId})`,
+    'crf7f_ois_asset_entra_dat_userCurrentOw@odata.bind': `crf7f_ois_asset_entra_dat_users(${rowId})`,
     crf7f_asset_item_status: 0,
   };
 
