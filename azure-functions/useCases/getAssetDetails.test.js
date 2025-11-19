5aad17d49daf2d0b1b8b2b95746ed0ad04c5350c
Zach D <zach.l.dickerson@gmail.com>
2025-11-19 06:46:05 -0700

    Expanded the test suite for getAssetDetails to cover more useful cases

--- azure-functions/useCases/getAssetDetails.test.js
+++ azure-functions/useCases/getAssetDetails.test.js
@@ -11,10 +11,51 @@ describe('getAssetDetails', () => {
     jest.clearAllMocks();
   });
 
+  //Basic tests to ensure the function is being called as expected
+
   it('calls getAssetByID using provided assetID (12345)', async () => {
     await getAssetDetails('asset123');
 
     expect(getAssetByID).toHaveBeenCalledTimes(1);
     expect(getAssetByID).toHaveBeenCalledWith('asset123');
+  });
+
+  //Test to see if it will return an expected object (without dataverse)
+
+  it('returns the resolved getAssetByID', async () => {
+    const testAsset = {
+      id: 'asset123',
+      deviceType: 'Laptop',
+      owner: 'Bob Ross',
+    };
+    getAssetByID.mockResolvedValue(testAsset);
+    const result = await getAssetDetails('asset123');
+    expect(result).toEqual(testAsset);
+  });
+
+  //Basic tests for expected failure states
+
+  it('throws the same errors expected from getAssetByID', async () => {
+    getAssetByID.mockRejectedValue(new Error('404: Asset not found'));
+    await expect(getAssetDetails('asset123')).rejects.toThrow(
+      '404: Asset not found',
+    );
+  });
+
+  it('handles getAssetByID returning null', async () => {
+    getAssetByID.mockResolvedValue(null);
+    const result = await getAssetDetails('asset123');
+    expect(result).toBeNull();
+  });
+
+  it('handles getAssetByID returning undefined', async () => {
+    getAssetByID.mockResolvedValue(undefined);
+    const result = await getAssetDetails('asset123');
+    expect(result).toBeUndefined();
+  });
+
+  it('gracefully handles calls without an assetID', async () => {
+    getAssetByID.mockRejectedValue(new Error('404: Asset not found'));
+    await expect(getAssetDetails()).rejects.toThrow('404: Asset not found');
   });
 });
