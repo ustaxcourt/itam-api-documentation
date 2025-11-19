2cb9a7952b4838767334e6cd97f58a73cb8aa8dd
Zach D <zach.l.dickerson@gmail.com>
2025-11-19 07:17:21 -0700

    expanded the test suite for assignAssetToUser

--- azure-functions/useCases/assignAssetToUser.test.js
+++ azure-functions/useCases/assignAssetToUser.test.js
@@ -11,10 +11,24 @@ describe('assignAssetToUser', () => {
     jest.clearAllMocks();
   });
 
+  //Basic test if function is being called correctly
   it('calls assignAssetOwner with the given userID and assetID', async () => {
     await assignAssetToUser('testUser', 'asset123');
 
     expect(assignAssetOwner).toHaveBeenCalledTimes(1);
     expect(assignAssetOwner).toHaveBeenCalledWith('testUser', 'asset123');
+  });
+
+  //Basic tests for expected failure modes
+  it('throws errors like assignAssetOwner', async () => {
+    assignAssetOwner.mockRejectedValue(new Error('XXX: user not found'));
+    await expect(assignAssetToUser('user123', 'asset123')).rejects.toThrow(
+      'XXX: user not found',
+    );
+  });
+
+  it('handles empty calls', async () => {
+    assignAssetOwner.mockRejectedValue(new Error('XXX: user not found'));
+    await expect(assignAssetToUser()).rejects.toThrow('XXX: user not found');
   });
 });
