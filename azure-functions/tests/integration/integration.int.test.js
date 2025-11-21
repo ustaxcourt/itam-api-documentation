52afa973643d85acdf88c838b1de0edca8dbae38
sharletclaros <23251609+sharletclaros@users.noreply.github.com>
2025-11-21 13:53:46 -0500

    set-up and outline for integration test

---
+++ azure-functions/tests/integration/integration.int.test.js
@@ -0,0 +1,20 @@
+const baseUrl = 'http://localhost:7071';
+const existingAssetId = '8da09331-b7b9-f011-bbd2-000d3a56dc3a';
+// const nonExistentAssetId = '00000000-0000-0000-0000-000000000000';
+// const existingUserId = '12345678-aaaa-bbbb-cccc-1234567890ab';
+// const nonExistentUserId = 'ffffffff-ffff-ffff-ffff-ffffffffffff';
+
+describe('Integration for ITAM Project', () => {
+  it('should remove assignment successfully', async () => {
+    const res = await fetch(
+      `${baseUrl}/api/v1/assets/${existingAssetId}/assignments`,
+      {
+        method: 'DELETE',
+        headers: { Authorization: 'Bearer mocked-token' },
+      },
+    );
+    expect(res.status).toBe(200);
+    const body = await res.json();
+    expect(body.message).toMatch(/Successfully updated item assignment/);
+  });
+});
