759a9311eb330fe9068971a6e9322cc34150f8d3
Zach D <zach.l.dickerson@gmail.com>
2025-11-21 09:50:02 -0700

    adjusted the persistence method based on Matt's postman call

--- azure-functions/persistence/getDataverseAssetsByEmail.js
+++ azure-functions/persistence/getDataverseAssetsByEmail.js
@@ -1,24 +1,16 @@
-/* Commenting things out so ESLint doesn't yell at me any more...
-import { dataverseCall } from "./dataverseCall";
-import { returnLookupID } from "./returnLookupID";
+import { dataverseCall } from './dataverseCall';
+import { filterDictionary } from './filterDict';
 
 export async function getDataverseAssetsByEmail(email) {
   try {
-    //Things to fix: ODataQuery, both TABLE calls, returnLookupID
     const { DATAVERSE_URL } = process.env;
-    const filter = {ODataQuery};
-    const user = await returnLookupID(ENTRA-TABLE, email, GUID);
-    const url = `${DATAVERSE_URL}/api/data/v9.2/${ASSET-TABLE}${filter}eq${user}`;
-
-    const data = dataverseCall(url, "GET");
-    const assets = data.value;
+    const url = `${DATAVERSE_URL}/api/data/v9.2/crf7f_ois_asset_rela_item_orgs?$filter=crf7f_ois_asset_entra_dat_userCurrentOw/crf7f_email eq ${email}&$expand=crf7f_ois_asset_entra_dat_userCurrentOw($select=crf7f_email,crf7f_jobtitle,crf7f_name,crf7f_isactive,crf7f_iscontractor, crf7f_location)`;
+    const data = dataverseCall(url, 'GET');
+    const assets = filterDictionary(data);
     return assets;
-
   } catch {
     //Maybe follow the pattern that Matt made to "bubble up" errors or throw a new "AppError"
-    //Hoping to handle Auth error (401>), bad query (badrequest?), and network issues (500)
-    throw new Error('Something went wrong lol')
+    //Hoping to handle Auth error (401?), bad query (badrequest?), and network issues (500)
+    throw new Error('Something went wrong lol');
   }
-
 }
-*/
