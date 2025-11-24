84d0adde96e1c20b9e60e7bb02a6297f185989d8
Zach D <zach.l.dickerson@gmail.com>
2025-11-24 12:05:57 -0700

    cut down email query logic since it was never being hit, corrected jest test for index, added error

--- azure-functions/apiController/queryAssetsByEmail.js
+++ azure-functions/apiController/queryAssetsByEmail.js
@@ -1,31 +1,26 @@
 import { app } from '@azure/functions';
 import { buildResponse } from './buildResponse.js';
 import { getAssetsByEmail } from '../useCases/getAssetsByEmail.js';
-import { BadRequest } from '../errors/BadRequest.js';
-//import { DataverseTokenError } from '../errors/DataverseTokenError.js';
+import { NotFoundError } from '../errors/NotFoundError.js';
 
 export async function queryAssetsByEmail(request) {
   try {
-    //Before doing any work, it's worth checking if there's even an email to use
     const email = request.params.email;
-    console.log(email);
-    if (!email) {
-      throw new BadRequest('Missing query parameter: email');
-    }
-
     const assets = await getAssetsByEmail(email);
+
     if (assets.length === 0) {
-      throw new BadRequest(`No assets found for provided email: ${email}`);
+      throw new NotFoundError(`No assets found for provided email: ${email}`);
     }
 
-    return await buildResponse(200, 'Success', assets);
+    return buildResponse(200, 'Success', assets);
   } catch (error) {
-    return await buildResponse(404, error.message);
+    console.log(`Encounted error: ${error.message}`);
+    return buildResponse(error.statusCode, error.message);
   }
 }
 app.http('queryAssetsByEmail', {
   methods: ['GET'],
   authLevel: 'anonymous',
-  route: 'api/v1/assets/userAssets/{email}',
+  route: 'v1/assets/userAssets/{email}',
   handler: queryAssetsByEmail,
 });
