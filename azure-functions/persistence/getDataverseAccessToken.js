5c9cdde88a71359cbfe1e331f12e810e8085188f
sharletclaros <23251609+sharletclaros@users.noreply.github.com>
2025-12-18 15:51:08 -0500

    update to dataverse token retrival to line up with infra naming

--- azure-functions/persistence/getDataverseAccessToken.js
+++ azure-functions/persistence/getDataverseAccessToken.js
@@ -1,12 +1,13 @@
 import { DataverseTokenError } from '../errors/DataverseTokenError.js';
 
 export async function getDataverseAccessToken() {
-  const { CLIENT_ID, TENANT_ID, DATAVERSE_INTERNAL, SCOPE } = process.env;
+  const { DATAVERSE_CLIENT_ID, TENANT_ID, DATAVERSE_INTERNAL, SCOPE } =
+    process.env;
 
   const tokenUrl = `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/token`;
 
   const params = new URLSearchParams();
-  params.append('client_id', CLIENT_ID);
+  params.append('client_id', DATAVERSE_CLIENT_ID);
   params.append('client_secret', DATAVERSE_INTERNAL);
   params.append('grant_type', 'client_credentials');
   params.append('scope', SCOPE);
