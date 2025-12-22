d8c64a137122afc51b06bb1f2d3e91a98d7bbac8
sharletclaros <23251609+sharletclaros@users.noreply.github.com>
2025-12-22 15:05:07 -0500

    removed stale value

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,4 @@
 locals {
-  env_ns = uuidv5("dns", var.env)
-
   sharepoint_app_id        = "00000007-0000-0000-c000-000000000000"
   graph_app_id             = "00000003-0000-0000-c000-000000000000"
   graph_user_read_scope_id = "e1fe6dd8-ba31-4d61-89e7-88639da4683d"
