291d5fd48ea99dd491214b52978a9b58c238b041
Zach D <zach.l.dickerson@gmail.com>
2025-12-29 11:47:13 -0700

    Converted buildResponse to TS and made it more generic, can refine later?

---
+++ azure-functions/apiController/buildResponse.ts
@@ -0,0 +1,21 @@
+//If more calls use this ApiResponse piece, we can make a centralized types file
+type ApiResponse<TData = unknown> = {
+  status: number;
+  jsonBody: {
+    message: string;
+    data: TData | null;
+  };
+};
+
+//TData can be anything that we're inputting, Object or Object[], without losing data shape as unknown
+//TData is still defaulted to a null if nothing is provided
+export function buildResponse<TData = unknown>(
+  status: number,
+  message: string,
+  data: TData | null = null,
+): ApiResponse<TData> {
+  return {
+    status,
+    jsonBody: { message, data },
+  };
+}
