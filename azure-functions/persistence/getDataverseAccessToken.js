9469a8737f864db636055ddc62c192605d386c1b
Zach D <zach.l.dickerson@gmail.com>
2025-12-31 06:07:11 -0700

    changed getDataverseAccessToken to point to PROD for testing, all tests pass

--- azure-functions/persistence/getDataverseAccessToken.js
+++ azure-functions/persistence/getDataverseAccessToken.js
@@ -1,16 +1,20 @@
 import { DataverseTokenError } from '../errors/DataverseTokenError';
 
 export async function getDataverseAccessToken() {
-  const { DATAVERSE_CLIENT_ID, TENANT_ID, DATAVERSE_INTERNAL, SCOPE } =
-    process.env;
+  const {
+    DATAVERSE_CLIENT_ID_PROD,
+    TENANT_ID,
+    DATAVERSE_INTERNAL_PROD,
+    SCOPE_DATAVERSE_LOCAL_PROD,
+  } = process.env;
 
   const tokenUrl = `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/token`;
 
   const params = new URLSearchParams();
-  params.append('client_id', DATAVERSE_CLIENT_ID);
-  params.append('client_secret', DATAVERSE_INTERNAL);
+  params.append('client_id', DATAVERSE_CLIENT_ID_PROD);
+  params.append('client_secret', DATAVERSE_INTERNAL_PROD);
   params.append('grant_type', 'client_credentials');
-  params.append('scope', SCOPE);
+  params.append('scope', SCOPE_DATAVERSE_LOCAL_PROD);
 
   try {
     const response = await fetch(tokenUrl, {
