cded7a088ab8a40eb180f88023a4fc8b522dd44c
mh137946 <Matthew.Hung.ctr@ustaxcourt.gov>
2025-12-31 08:24:39 -0800

    updated tests to reflect query change

--- azure-functions/persistence/getJobTitleDefaultRequirements.js
+++ azure-functions/persistence/getJobTitleDefaultRequirements.js
@@ -6,7 +6,7 @@ import { parseDataverseResponse } from './parseDataverseResponse.js';
 export async function getJobTitleDefaultRequirements() {
   // Build Dataverse query URL
 
-  const query = `crf7f_ois_job_title_model_types?$select=crf7f_modelmaximum,crf7f_modelminimum,crf7f_JobTitleAssetType,crf7f_ReferenceModel&$filter=crf7f_JobTitleAssetType/crf7f_isdefault eq true&$expand=crf7f_ReferenceModel($select=crf7f_name),crf7f_JobTitleAssetType($select=crf7f_minimumquanitity,crf7f_maximumquantity,crf7f_JobTitle,crf7f_AssetType;$expand=crf7f_JobTitle($select=crf7f_title),crf7f_AssetType($select=crf7f_name))`;
+  const query = `crf7f_ois_job_title_model_types?$select=crf7f_modelmaximum,crf7f_modelminimum,crf7f_JobTitleAssetType,crf7f_ReferenceModel&$filter=crf7f_JobTitleAssetType/crf7f_isdefault eq true &$expand=crf7f_ReferenceModel($select=crf7f_name),crf7f_JobTitleAssetType($select=crf7f_minimumquanitity,crf7f_maximumquantity,crf7f_JobTitle,crf7f_AssetType;$expand=crf7f_JobTitle($select=crf7f_title),crf7f_AssetType($select=crf7f_name))`;
 
   // Dataverse call
   const response = await dataverseCall({ query: query, method: 'GET' });
