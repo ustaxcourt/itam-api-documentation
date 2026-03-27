c5313405e5e6e759631a6262eee06f5c19cc4f08
sharletclaros <23251609+sharletclaros@users.noreply.github.com>
2026-03-27 14:56:50 -0400

    implemented query handling for decommissed assets

--- azure-functions/useCases/getAssetDetails.js
+++ azure-functions/useCases/getAssetDetails.js
@@ -1,6 +1,14 @@
 import { getAssetByID } from '../persistence/getAssetByID.js';
+import { checkDecommissioned } from '../persistence/checkDecommissioned.js';
+import { NotFoundError } from '../errors/NotFoundError.js';
 
 export async function getAssetDetails(id) {
+  // First let us check if the asset is decommissioned
+  const decommissioned = await checkDecommissioned(id);
+
+  if (decommissioned) {
+    throw new NotFoundError('This asset has been decommissioned.');
+  }
   // From persistence layer
   const asset = await getAssetByID(id);
 
